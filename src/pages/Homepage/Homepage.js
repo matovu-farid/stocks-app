e3f541cdac835cface32a171581f1fabcfab9329
matovu-farid <matovu90@gmail.com>
2022-03-07 15:36:27 +0300

    Render dummy data

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,3 +1,38 @@
-const Homepage = () => <p>Hello world</p>;
-
+const data = [
+  {
+    symbol: 'AAPL',
+    companyName: 'Apple Inc.',
+    price: 163.1700,
+  },
+  {
+    symbol: 'NVDA',
+    companyName: 'NVIDIA Corporation',
+    price: 229.3600,
+  },
+  {
+    symbol: 'AVGO',
+    companyName: 'Broadcom Inc.',
+    price: 595.9900,
+  },
+  {
+    symbol: 'ADBE',
+    companyName: 'Adobe Inc.',
+    price: 452.1300,
+  },
+];
+const Homepage = () => (
+  <ul>
+    {
+      data.map(({ symbol, companyName, price }) => (
+        <li key={symbol}>
+          <div>
+            <p>{symbol}</p>
+            <p>{companyName}</p>
+            <p>{price}</p>
+          </div>
+        </li>
+      ))
+    }
+  </ul>
+);
 export default Homepage;
