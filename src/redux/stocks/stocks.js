9cbfdff6c60c254719a5c4745084c4547bbab30c
matovu-farid <matovu90@gmail.com>
2022-03-07 14:41:18 +0300

    Fix: linting errors

---
+++ src/redux/stocks/stocks.js
@@ -0,0 +1,24 @@
+const FETCHDATA = 'STOCKS/FETCH';
+
+const url = 'https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=9b6100842231c66b4c6c23e323384b39';
+export const fetchData = () => async (dispatch) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const payload = data.map(({
+    id, rocket_name: name, description, flickr_images: images,
+  }) => ({
+    id, name, description, images, reserved: false,
+  }));
+  dispatch({
+    type: FETCHDATA,
+    payload,
+  });
+};
+
+const stocksReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCHDATA: return action.payload;
+    default: return state;
+  }
+};
+export default stocksReducer;
