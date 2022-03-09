a289fc8b4cd49a0949bcea7103b22798926b1a8d
matovu-farid <matovu90@gmail.com>
2022-03-09 16:51:23 +0300

    Style with images

--- src/redux/company/company.js
+++ src/redux/company/company.js
@@ -1,7 +1,9 @@
+import apikey from '../apikey';
+
 const FETCHCOMPANY = 'COMPANY/FETCH';
 const CLEARCOMPANY = 'COMPANY/CLEAR';
 export const fetchCompany = (symbol) => async (dispatch) => {
-  const url = `https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=9b6100842231c66b4c6c23e323384b39`;
+  const url = `https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=${apikey}`;
   const payload = (await (await fetch(url)).json())[0];
   dispatch({
     type: FETCHCOMPANY,
